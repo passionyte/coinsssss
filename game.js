cf44496b939874bd13e15439c12359e03c6b084d
passionyte <logan.valenz07@gmail.com>
2025-01-25 00:55:04 -0500

    New structures and upgrades, a few minor rebalances

--- game.js
+++ game.js
@@ -34,7 +34,9 @@ const items = {
         Factory = {Name: "Factory", Cost: 14000, CoinsPs: 128, Description: "Why not build a Factory that produces coins?"},
         EightBall = {Name: "8-Ball", Cost: 88000, CoinsPs: 512, Description: "What's the point of being conventional? Start bulk buying 8-Balls for more coin wishes."},
         Currency = {Name: "Currency", Cost: 500000, CoinsPs: 2800, Description: "Now you must mean serious business. Convince people to start paying solely in coins."},
-        Alchemy = {Name: "Alchemy Lab", Cost: 3200000, CoinsPs: 9999, Description: "Turns cookies into gold coins. Seems worth it, right?"},
+        Research = {Name: "Research Facility", Cost: 3200000, CoinsPs: 12000, Description: "Research facilities which prioritize finding new ways to make coins."},
+        MatterRefiner = {Name: "Matter Refiner", Cost: 48000000, CoinsPs: 50000, Description: "Developed by your research facilities, you can now refine matter to the point where it turns into coins."},
+        Matrix = {Name: "The Matrix", Cost: 600000000, CoinsPs: 400000, Description: "Didn't see this coming, did you? Now choose a coin... red or blue..."}
     ],
     upgrades: [
         DoubleClick = {Name: "Double Click", Cost: 100, CoinsPc: 1, Description: "Doubles your coins per click!"},
@@ -54,17 +56,29 @@ const items = {
         Quantum = {Name: "Quantum Mechanics", Cost: 400000, StructName: "Factory", StructMult: 2, Description: "627% more efficient than conventional electricity... Factories are twice as efficient!", Requirements: {Structures: {Factory: 10}}},
         EightBallClick2 = {Name: "8-Ball Click Mk2", Cost: 88888, CoinsPcPs: 0.03, Description: "Yeah, that's right; ask your personal 8-Ball for another coin related wish. This is your last... Clicking earns 3% of your coins per second!", Requirements: {Stats: {CoinsPcPs: 0.02}}},
         BronzeFortune = {Name: "Bronze Fortune", Cost: 25000, CoinsPsMult: 0.1, Description: "Not the best, but better than nothing. Gives 10% production multiplier."},
-        DiamondPickaxe = {Name: "Diamond Pickaxe", Cost: 15000, StructName: "Miner", StructMult: 2, Description: "Upgrade pickaxes from Gold to Diamond. Miners are twice as efficient!", Requirements: {Structures: {Miner: 25}}},
+        PlatinumPickaxe = {Name: "Platinum Pickaxe", Cost: 15000, StructName: "Miner", StructMult: 2, Description: "Upgrade pickaxes from Gold to Platinum. (Please tell me how this is efficient...) Miners are twice as efficient!", Requirements: {Structures: {Miner: 25}}},
         SteelMouse = {Name: "Steel Mouse", Cost: 9001, StructName: "Clicker", StructMult: 2, Description: "Clink clink clink... Clickers are twice as efficient!", Requirements: {Structures: {Clicker: 25}}},
         Crypt = {Name: "Crypto", Cost: 33000, StructName: "Trader", StructMult: 2, Description: "Yeah. Now these deals make even more sense. Totally. Traders are twice as efficient!", Requirements: {Structures: {Trader: 25}}},
         CoinApprovedClick = {Name: "Coin-Approved Click", Cost: 150000, CoinsPc: 12, Multiply: true, Description: "The name speaks for itself. Base coins per click is multiplied by 12.", Requirements: {Stats: {CoinsPc: 8}}},
         SuperFullTime = {Name: "Super Full-Time", Cost: 400000, StructName: "Business", StructMult: 2, Description: "So workers thought full-time was a lot? You say: Nah. Businesses are twice as efficient!", Requirements: {Structures: {Business: 25}}},
         Nuclear = {Name: "Nuclear Mechanics", Cost: 1000000, StructName: "Factory", StructMult: 2, Description: "Radioactive. Factories are twice as efficient!", Requirements: {Structures: {Factory: 25}}},
-        SilverFortune = {Name: "Silver Fortune", Cost: 1000000, CoinsPsMult: 0.25, Description: "'Second is the best' Gives 25% production multiplier.", Requirements: {Stats: {CoinsPsMult: 1.1}}},
+        SilverFortune = {Name: "Silver Fortune", Cost: 1000000, CoinsPsMult: 0.25, Description: "'Second is the best' Gives 15% production multiplier.", Requirements: {Stats: {CoinsPsMult: 1.1}}},
         EightSquared = {Name: "8^2", Cost: 900000, StructName: "8-Ball", StructMult: 2, Description: "8 squared is 64! 8-Balls are twice as efficient!", Requirements: {Structures: {["8-Ball"]: 1}}},
         EightCubed = {Name: "8^3", Cost: 4000000, StructName: "8-Ball", StructMult: 2, Description: "8 cubed is 512! 8-Balls are twice as efficient!", Requirements: {Structures: {["8-Ball"]: 10}}},
         Investors = {Name: "Investors", Cost: 2000000, StructName: "Currency", StructMult: 2, Description: "Get top of the line investors to well.. endorse coins! Currencies are twice as efficient!", Requirements: {Structures: {Currency: 1}}},
-        Supercharged = {Name: "Supercharged", Cost: 6000000, StructName: "Alchemy Lab", StructMult: 2, Description: "Supercharged SCIENCE! Alchemy Labs are twice as efficient!", Requirements: {Structures: {["Alchemy Lab"]: 1}}},
+        Supercharged = {Name: "Supercharged", Cost: 6000000, StructName: "Research Facility", StructMult: 2, Description: "Supercharged SCIENCE! Research Facilities are twice as efficient!", Requirements: {Structures: {["Research Facility"]: 1}}},
+        GoldFortune = {Name: "Gold Fortune", Cost: 24000000, CoinsPsMult: 0.25, Description: "Well we all SHIIIIIINE onnnnn! Gives 25% production multiplier.", Requirements: {Stats: {CoinsPsMult: 1.25}}},
+        PlatinumFortune = {Name: "Platinum Fortune", Cost: 400000000, CoinsPsMult: 0.5, Description: "Technically white gold... Actually, probably not. Gives 50% production multiplier.", Requirements: {Stats: {CoinsPsMult: 1.5}}},
+        DiamondFortune = {Name: "Diamond Fortune", Cost: 9000000001, CoinsPsMult: 1, Description: "Didn't think you'd get this far, so here, take this! Gives 100% production multiplier.", Requirements: {Stats: {CoinsPsMult: 2}}},
+        BrandEndorse = {Name: "Brand Endorsements", Cost: 16000000, StructName: "Currency", StructMult: 2, Description: "Why have investors endorse your coins when you can have brands do the same? Currencies are twice as efficient!", Requirements: {Structures: {Currency: 10}}},
+        DiamondPickaxe = {Name: "Diamond Pickaxe", Cost: 400000, StructName: "Miner", StructMult: 2, Description: "Upgrade pickaxes from Platinum to Diamond. MINING AWAY... Miners are twice as efficient!", Requirements: {Structures: {Miner: 50}}},
+        ObsidianMouse = {Name: "Obsidian Mouse", Cost: 54000, StructName: "Clicker", StructMult: 2, Description: "Clonck clonck clonck... Patience is all you need... Clickers are twice as efficient!", Requirements: {Structures: {Clicker: 50}}},
+        Collaboration = {Name: "Collaboration", Cost: 1200000, StructName: "Trader", StructMult: 2, Description: "Well, judging by the amount of Traders you've acquired and the price of this upgrade... We'd just assume that you have a currency going. Why not collaborate! Traders are twice as efficient!", Requirements: {Structures: {Trader: 50}}},
+        SuperUnion = {Name: "Super Union", Cost: 9900000, StructName: "Business", StructMult: 2, Description: "A union that is pro-working Super Full-Time hours! Businesses are twice as efficient!", Requirements: {Structures: {Business: 50}}},
+        Atomic = {Name: "Atomic Mechanics", Cost: 26000000, StructName: "Factory", StructMult: 2, Description: "ATOMIC. Factories are twice as efficient!", Requirements: {Structures: {Factory: 50}}},
+        YeahScience = {Name: "YEAH SCIENCE!", Cost: 52000000, StructName: "Research Facility", StructMult: 2, Description: "Just go berzerk with your science. Research Facilities are twice as efficient!", Requirements: {Structures: {["Research Facility"]: 10}}},
+        Optimization = {Name: "Optimization", Cost: 168000000, StructName: "Matter Refiner", StructMult: 2, Description: "Make some absurdly expensive optimizations to your Matter Refiners for 'maximum' coin output. Matter Refiners are twice as efficient!", Requirements: {Structures: {["Matter Refiner"]: 1}}},
+        AdvancedPrograms = {Name: "Advanced Programs", Cost: 2140000000, StructName: "The Matrix", StructMult: 2, Description: "Get your tech gurus from the Research Facilities and in the Matrix to design better programs for COINS... And Only COINS... The Matrixes are twice as efficient!", Requirements: {Structures: {["The Matrix"]: 1}}}
     ]
 }
 
